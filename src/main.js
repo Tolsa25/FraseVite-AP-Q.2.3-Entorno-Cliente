ccf3557b04b9021e15bd5f6114a8305c29e72f4d
kevin <judoka225@gmail.com>
2025-10-22 17:12:19 +0200

    Corregidos errores en el main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,38 @@
-import { letters, words, upperString, titleString, backwardsLetters, backwardsWords, palindrome } from './functions.js'
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import {
+    letters,
+    words,
+    upperString,
+    titleString,
+    backwardsLetters,
+    backwardsWords,
+    palindrome
+} from './functions.js';
+
+
+function resultado(){
+  const frase = prompt("Introduce una frase:").trim();
+  console.log(`Frase introducida: "${frase}"`);
+
+    const numLetrasSinEspacios = letters(frase.replace(/\s/g, ''));
+    const numPalabras = words(frase);
+    console.log(`1. Numero de letras: ${numLetrasSinEspacios}`);
+    console.log(`2. Numero de palabras: ${numPalabras}`);
+
+    console.log(`3. Frase en mayusculas: ${upperString(frase)}`);
+
+    console.log(`4. Título (Mayusculas por palabra): ${titleString(frase)}`);
+
+    console.log(`5. Letras al reves: ${backwardsLetters(frase)}`);
+
+    console.log(`6. Palabras al revés: ${backwardsWords(frase)}`);
+
+    const esPalindromo = palindrome(frase);
+    console.log(`7. ¿Es palíndromo?: ${esPalindromo ? 'SÍ' : 'NO'}`);
+};
+
+resultado();
+
 
 document.querySelector('#app').innerHTML = `
   <!doctype html>
